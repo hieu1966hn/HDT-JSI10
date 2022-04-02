148ccc7a4e103c852861dee52ee5450e1fcce26a
hieu1966hn <hieu1966hn@gmail.com>
2022-04-02 18:18:39 +0700

    done khai báo mảng

---
+++ SS1/app.js
@@ -0,0 +1,22 @@
+let student = [
+  {
+    name: "NVA",
+    age: 16,
+    course: ["mindX 01", "mindX 02", "mindX 03"]
+  },
+  {
+    name: "NVB",
+    age: 18,
+    course: ["mindX 01", "mindX 02", "mindX 04"]
+  },
+  {
+    name: "NVC",
+    age: 17,
+    course: ["mindX 01", "mindX 03", "mindX 04"]
+  },
+  {
+    name: "NVD",
+    age: 15,
+    course: ["mindX 01", "mindX 02", "mindX 03"]
+  }
+]
