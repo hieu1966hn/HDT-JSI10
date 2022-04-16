142b9acfe894c94da137637f44224bdd9516708f
hieu1966hn <hieu1966hn@gmail.com>
2022-04-16 18:15:41 +0700

    done assign

--- SS2/app.js
+++ SS2/app.js
@@ -1,23 +1,51 @@
-let obj = {
-  name: "John",
-  age: 24,
-  address: '22 Jump Street'
+// let obj = {
+//   name: "John",
+//   age: 24,
+//   address: '22 Jump Street'
+// }
+
+// /// sao chép đối tượng này vào một biến  có tên là obj2;
+// let obj2 = {
+//   name: "Hiếu",
+//   age: 11,
+//   hobbies: "Killed"
+// }
+
+// let obj3 = Object.assign(obj, obj2)
+
+
+// console.log(obj3); // => 4 thuộc tính 
+
+// /*
+// Object.assign(target1, target2,.... source )
+
+// => Object có tất cả các thuộc tính từ đối tượng khác
+// */
+
+
+
+//// VD mới về Object.assign
+
+let MĐ = {
+  name: "Minh Đức",
+  age: 17,
+  hobbies: 'Null',
+  DucID: "001"
 }
 
-/// sao chép đối tượng này vào một biến  có tên là obj2;
-let obj2 = {
-  name: "Hiếu",
-  age: 11,
-  hobbies: "Killed"
+
+let BK = {
+  name: "Bảo Khánh",
+  age: 18,
+  hobbies: 'Anime',
+  KhanhID: "002"
+}
+let VT = {
+  name: "Vũ Tùng",
+  age: 17,
+  hobbies: 'Many',
+  TungID: "003"
 }
 
-let obj3 = Object.assign(obj, obj2)
-
-
-console.log(obj3); // => 4 thuộc tính 
-
-/*
-Object.assign(target1, target2,.... source )
-
-=> Object có tất cả các thuộc tính từ đối tượng khác
-*/
+let hdt_jsi10 = Object.assign(MĐ, BK, VT);
+console.log("hdt_jsi10: ", hdt_jsi10); // => {}
