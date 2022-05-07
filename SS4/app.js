ae67a0566e82f399ff9e6bc1ce8b44590e7a8c72
hieu1966hn <hieu1966hn@gmail.com>
2022-05-07 18:32:28 +0700

    done kế thừa

--- SS4/app.js
+++ SS4/app.js
@@ -15,23 +15,52 @@
 
 
 // 2. Thuộc tính của class trong Javascript
-class Person {
-  // khai báo thuộc tính
-  constructor(_name, _age, _address, _hobbies) {
-    this.name = _name;
-    this.age = _age;
-    this.address = _address;
-    this.hobbies = _hobbies
+// class Person {
+//   // khai báo thuộc tính
+//   constructor(_name, _age, _address, _hobbies) {
+//     this.name = _name;
+//     this.age = _age;
+//     this.address = _address;
+//     this.hobbies = _hobbies
 
+//   }
+//   // constructor: là 1 phương thức khởi tạo mặc định của lớp
+
+//   say() {
+//     return "Hello I am a human"
+//   }
+
+// }
+
+// let me = new Person("Nguyễn Trung Hiếu", 25, "Tây Hô - Hà Nội", "coding, sleeping");
+// console.log("me: ", me);
+// console.log(me.name);//?
+
+
+// 3. Tính kế thừa
+class Person {
+  /// khai báo thuộc tính của lớp
+  constructor(_name, _age) {
+    this.name = _name
+    this.age = _age
   }
-  // constructor: là 1 phương thức khởi tạo mặc định của lớp
 
   say() {
-    return "Hello I am a human"
+    return `I am Person and my name is ${this.name}`
   }
-
 }
 
-let me = new Person("Nguyễn Trung Hiếu", 25, "Tây Hô - Hà Nội", "coding, sleeping");
-console.log("me: ", me);
+// Khai báo clas Student kế thừa từ clas Person
+class Student extends Person {
+  constructor(_name, _age, _mhs) {
+    super(_name, _age); // Kế thừa thuộc tính name, age đã khai báo của cha nó
+    this.mhs = _mhs
+  }
+}
+
+let VuTung = new Student("Đoàn Vũ Tùng", 17, "1234567890");
+console.log("VuTung: ", VuTung);
+console.log(VuTung.say());
+
+
 
