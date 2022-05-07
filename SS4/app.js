4a2eef785cf2cfc8599113f9e9e8382d102be6ca
hieu1966hn <hieu1966hn@gmail.com>
2022-05-07 18:14:26 +0700

    done truyền biến thuộc tính vào class với constructor

--- SS4/app.js
+++ SS4/app.js
@@ -1,13 +1,37 @@
 // tham khảo trước cú pháp OOP trong JS
 
 // 1. Khởi tạo class trong JS
+// class Person {
 
+//   // Khai báo phương thức say
+//   say() {
+//     alert("T đang nói đấy");
+//   }
+// }
+
+// /// Áp dụng class
+// let HuyBinh = new Person();
+// HuyBinh.say();
+
+
+// 2. Thuộc tính của class trong Javascript
 class Person {
-  say() {
-    alert("T đang nói đấy");
+  // khai báo thuộc tính
+  constructor(_name, _age, _address, _hobbies) {
+    this.name = _name;
+    this.age = _age;
+    this.address = _address;
+    this.hobbies = _hobbies
+
   }
+  // constructor: là 1 phương thức khởi tạo mặc định của lớp
+
+  say() {
+    return "Hello I am a human"
+  }
+
 }
 
-/// Áp dụng class
-let HuyBinh = new Person();
-HuyBinh.say();
+let me = new Person("Nguyễn Trung Hiếu", 25, "Tây Hô - Hà Nội", "coding, sleeping");
+console.log("me: ", me);
+
