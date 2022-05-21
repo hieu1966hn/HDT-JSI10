eb6d1d3f372a116ffcdedafdc56574cce69dad6e
hieu1966hn <hieu1966hn@gmail.com>
2022-05-21 19:02:15 +0700

    done code create file js

--- SS5+SS6-Web-chat/js/index.js
+++ SS5+SS6-Web-chat/js/index.js
@@ -1,36 +1,7 @@
 const init = () => {
   // alert("Hàm init đã được chạy");
 
-  const registerForm = document.getElementById("register-form");
-  const loginForm = document.getElementById("login-form");
 
-  // document.getElementById("redirect-to-login").
-  //   addEventListener("click", function () {
-  //     window.location.href = '../html/login.html'
-  //   })
-
-
-  // document.getElementById("redirect-to-register").
-  //   addEventListener("click", function () {
-  //     window.location.href = '../html/register.html'
-  //   })
-
-  registerForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // giải quyết vđề tự động reload web
-
-    const data = {
-      firstName: registerForm.firstName.value,
-      lastName: registerForm.lastName.value,
-      email: registerForm.email.value,
-      password: registerForm.password.value,
-      confirmPassword: registerForm.confirmPassword.value
-    }
-
-    console.log(data);
-
-    controller.register(data);
-    // Kiểm tra lỗi người dùng type tại trang đăng ký
-  })
 
 }
 
