753fc78bcc90cb0bb70d1e8c95e81bea3245bde6
hieu1966hn <hieu1966hn@gmail.com>
2022-05-28 19:17:16 +0700

    Done login

--- SS7/login.js
+++ SS7/login.js
@@ -3,11 +3,25 @@ const loginForm = document.getElementById("login-form");
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const data = {
+  const dataLogin = {
     email: loginForm.email.value,
     password: loginForm.password.value,
 
   }
 
-  console.log(data);
+  console.log(dataLogin);
+
+  const login = async (dataLogin) => {
+    try {
+      const response = await firebase.auth().signInWithEmailAndPassword(dataLogin.email, dataLogin.password);
+      alert("Nguoi dung dang nhap thanh cong")
+      location.href = "./trangchu.html"
+    }
+    catch (err) {
+      console.log(err);
+      alert(err.message);
+    }
+  }
+
+  login(dataLogin)
 })
